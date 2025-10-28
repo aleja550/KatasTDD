3363316cb328f587082b33fa6c6a447a4b736323
Alejandra Sandoval <paula.sandoval@sinco.com.co>
2025-10-27 20:26:06 -0500

    🟢Cancion_Debe_Tener_12_Estrofas

--- CancionNavidad/Cancion.cs
+++ CancionNavidad/Cancion.cs
@@ -2,5 +2,14 @@ namespace CancionNavidad;
 
 public class Cancion
 {
-    public List<Estrofa> Estrofas { get; } = [];
+    public List<Estrofa> Estrofas { get; }
+    
+    public Cancion()
+    {
+        Estrofas = [];
+        for (var i = 0; i < 12; i++)
+        {
+            Estrofas.Add(new Estrofa());
+        }
+    }
 }
