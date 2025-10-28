6e6fd436bd00c1930817dcd60b9d65a984b23546
Alejandra Sandoval <paula.sandoval@sinco.com.co>
2025-10-27 20:46:29 -0500

    🔴PrimeraLinea_DeEstrofa1_Debe_Ser_OnTheFirstDayOfChristmas

---
+++ CancionNavidad/CancionNavidadTest.cs
@@ -0,0 +1,46 @@
+using AwesomeAssertions;
+
+namespace CancionNavidad;
+
+public class CancionNavidadTest
+{
+    [Fact]
+    public void Cancion_Debe_Tener_12_Estrofas()
+    {
+        //Arrange
+        var cancion = new Cancion();
+        
+        //Act
+        var cantidadEstrofas = cancion.Estrofas.Count;
+        
+        //Assert
+        cantidadEstrofas.Should().Be(12);
+    }
+    
+    [Fact]
+    public void PrimeraEstrofa_Debe_Tener_3_Lineas()
+    {
+        //Arrange
+        var cancion = new Cancion();
+    
+        //Act
+        var cantidadLineas = cancion.Estrofas[0].Lineas.Count;
+    
+        //Assert
+        cantidadLineas.Should().Be(3);
+    }
+    
+    [Fact]
+    public void PrimeraLinea_DeEstrofa1_Debe_Ser_OnTheFirstDayOfChristmas()
+    {
+        //Arrange
+        var cancion = new Cancion();
+        var textoEsperado = "On the first day of Christmas";
+    
+        //Act
+        var primeraLinea = cancion.Estrofas[0].Lineas[0];
+    
+        //Assert
+        primeraLinea.Should().Be(textoEsperado);
+    }
+}
