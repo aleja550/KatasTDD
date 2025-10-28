c641af82364183d661c944780e25a77a90da6506
Alejandra Sandoval <paula.sandoval@sinco.com.co>
2025-10-27 21:41:03 -0500

    🟢TodasLasEstrofas_Deben_Empezar_Con_OnTheDayOfChristmas

--- CancionNavidad/Estrofa.cs
+++ CancionNavidad/Estrofa.cs
@@ -3,29 +3,22 @@ namespace CancionNavidad;
 public class Estrofa
 {
     public List<string> Lineas { get; }
+    
+    private static readonly string[] Valores = 
+    {
+        "first", "second", "third", "fourth", "fifth", "sixth",
+        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
+    };
 
+    
     public Estrofa(int numeroEstrofa)
     {
         Lineas = new List<string>();
         
-        if (numeroEstrofa == 1)
-        {
-            Lineas.Add("On the first day of Christmas");
-            Lineas.Add("My true love sent to me:");
-            Lineas.Add("A partridge in a pear tree.");
-        }
-        else if (numeroEstrofa == 2)
-        {
-            Lineas.Add("On the second day of Christmas");
-            Lineas.Add("My true love sent to me:");
-            Lineas.Add("Two turtle doves and");
-            Lineas.Add("A partridge in a pear tree.");
-        }
-        else
-        {
-            Lineas.Add("");
-            Lineas.Add("");
-            Lineas.Add(""); 
-        }
+        Lineas.Add($"On the {Valores[numeroEstrofa - 1]} day of Christmas");
+        
+        Lineas.Add("My true love sent to me:");
+        
+        Lineas.Add("A partridge in a pear tree.");
     }
 }
