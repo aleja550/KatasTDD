f4e10bb87897a74036b11ebc35f7d81ca0ae92f8
Alejandra Sandoval <pa.sandoval@uniandes.edu.co>
2025-11-03 23:26:10 -0500

    🔵Convertir5_Debe_RetornarV

--- NumerosRomanos/NumeroRomano.cs
+++ NumerosRomanos/NumeroRomano.cs
@@ -4,9 +4,6 @@ public static class NumeroRomano
 {
     public static string Convertir(int numero)
     {
-        if (numero == 1)
-            return "I";
-
-        return "V";
+        return numero == 1 ? "I" : "V";
     }
 }
